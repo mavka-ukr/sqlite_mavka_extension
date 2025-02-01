02b0d947061c926850fcc274b19cb0fda64642b1
Davyd Kohut <davyd.kohut@gmail.com>
2025-02-01 22:57:41 +0200

    🧬

---
+++ збудувати.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+set -e
+set -x
+
+export CC="clang"
+export CXX="clang++"
+export AR="llvm-ar"
+export RANLIB="llvm-ranlib"
+export TSIL="ціль"
+
+$TSIL розширення_мавки_sqlite.ll скомпілювати розширення_мавки_sqlite.ц
+
+$CC -shared -fPIC \
+     -march=native \
+     -o sqlite/sqlite3.o \
+     sqlite/sqlite3.c
+
+$CXX -shared -fPIC \
+  -march=native \
+  -std=c++20 \
+  -flto \
+  -o розширення_мавки_sqlite.so \
+  розширення_мавки_sqlite.ll sqlite_mavka_extension.cpp sqlite/sqlite3.o
