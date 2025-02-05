edb17063b23f4c4636f8076391c7cacff3ed2890
Davyd Kohut <davyd.kohut@gmail.com>
2025-02-05 04:53:30 +0200

    🧬

--- sqlite_mavka_extension.cpp
+++ sqlite_mavka_extension.cpp
@@ -3,19 +3,19 @@
 #include "sqlite/sqlite3.h"
 #include <iostream>
 
-extern "C" void *sqlite_mavka_extension_open(char *path, size_t path_size) {
+extern "C" void *sqlite_mavka_extension_open(char *path, size_t path_size, char **error, size_t *error_size) {
     char *path_str = (char *) malloc(path_size + 1);
     memcpy(path_str, path, path_size);
     path_str[path_size] = '\0';
 
-    sqlite3 *db;
-    char *zErrMsg = 0;
-    int rc;
+    sqlite3 *db = nullptr;
 
-    rc = sqlite3_open(path_str, &db);
+    int rc = sqlite3_open(path_str, &db);
     free(path_str);
 
     if (rc) {
+        *error = strdup("cannot open database");
+        *error_size = strlen(*error);
         return nullptr;
     }
 
@@ -43,4 +43,42 @@ sqlite_mavka_extension_exec(void *db, char *sql, size_t sql_size, SqliteExecCall
     }
 
     return 1;
+}
+
+extern "C" void *sqlite_mavka_extension_prepare_v2(void *db, char *sql, size_t sql_size) {
+    char *sql_str = (char *) malloc(sql_size + 1);
+    memcpy(sql_str, sql, sql_size);
+    sql_str[sql_size] = '\0';
+
+    sqlite3_stmt *stmt;
+    int rc = sqlite3_prepare_v2((sqlite3 *) db, sql_str, sql_size, &stmt, nullptr);
+
+    free(sql_str);
+
+    if (rc != SQLITE_OK) {
+        return nullptr;
+    }
+
+    return stmt;
+}
+
+extern "C" int sqlite_mavka_extension_bind_double(void *stmt, int i, double value) {
+    return sqlite3_bind_double((sqlite3_stmt *) stmt, i, value);
+}
+
+extern "C" int sqlite_mavka_extension_bind_int(void *stmt, int i, int value) {
+    return sqlite3_bind_int((sqlite3_stmt *) stmt, i, value);
+}
+
+extern "C" int sqlite_mavka_extension_bind_int64(void *stmt, int i, long value) {
+    return sqlite3_bind_int64((sqlite3_stmt *) stmt, i, value);
+}
+
+extern "C" int sqlite_mavka_extension_bind_null(void *stmt, int i) {
+    return sqlite3_bind_null((sqlite3_stmt *) stmt, i);
+}
+
+extern "C" size_t sqlite_mavka_extension_expanded_sql(void *stmt, char **out) {
+    *out = sqlite3_expanded_sql((sqlite3_stmt *) stmt);
+    return strlen(*out);
 }
